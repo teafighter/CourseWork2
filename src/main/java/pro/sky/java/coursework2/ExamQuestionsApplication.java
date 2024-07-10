f1ad55cf50c37ec04d14bd7621e60b9ae23c1ff4
Kirill Tyurikov <teafighter@gmail.com>
2024-07-10 13:34:04 +0300

    initial commit

---
+++ src/main/java/pro/sky/java/coursework2/ExamQuestionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.coursework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionsApplication.class, args);
+	}
+
+}
